a84c910774601054811021cb81c440e2206a5c53
Soren Bear <soren.bear@gmail.com>
2022-07-25 10:23:44 -0700

    0.1.3

--- src/_meta/__init__.py
+++ src/_meta/__init__.py
@@ -6,5 +6,5 @@ __author__ = 'Soren Bear'
 __email__ = 'sbear@ucsb.edu'
 __copyright__ = f"2022, {__author__}"
 __license__ = 'MIT'
-__version__= '0.1.2'
+__version__= '0.1.3'
 __repo_url__ = 'https://github.com/sorenbear/magentropy'
