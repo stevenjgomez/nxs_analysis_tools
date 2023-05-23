a1439ad7de157ff703fa85efeecdd6d5aaec0605
Steven Gomez Alvarado <stevenjgomez@ucsb.edu>
2023-05-23 16:24:26 -0700

    Cleaned datareduction.py using pylint.

--- src/nxs_analysis_tools/datareduction.py
+++ src/nxs_analysis_tools/datareduction.py
@@ -1,24 +1,26 @@
+"""
+Reduces scattering data into 2D and 1D datasets.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.transforms import Affine2D
 from matplotlib.markers import MarkerStyle
-from IPython.display import display, Markdown, Latex
 from matplotlib.ticker import MultipleLocator
 import matplotlib.colors as colors
+from IPython.display import display, Markdown
 
-def plot_slice(X, Y, Z, vmin=None, vmax=None, skew_angle=90, ax=None,
-              xlim=None, ylim=None,
-              xticks=None, yticks=None,
-              cbar=True,
-              logscale=False,
-              symlogscale=False,
-              cmap='viridis',
-              linthresh = 1,
-              title=None,
-             ):
+
+
+def plot_slice(X, Y, Z, vmin=None, vmax=None, skew_angle=90, ax=None, xlim=None, ylim=None,
+    xticks=None, yticks=None, cbar=True, logscale=False, symlogscale=False, cmap='viridis', linthresh = 1, title=None):
     
     """
-    This function plots a 2D heatmap of a given dataset (Z) with the possibility of applying logarithmic or symmetrical logarithmic scales to the colorbar. The plot can be sheared to create a trapezoidal view with a defined skew angle. Tick labels are displayed on all sides of the plot. The function takes the following parameters:
+    This function plots a 2D heatmap of a given dataset (Z) with the possibility
+    of applying logarithmic or symmetrical logarithmic scales to the colorbar.
+    The plot can be sheared to create a trapezoidal view with a defined skew angle.
+    Tick labels are displayed on all sides of the plot. 
+    The function takes the following parameters:
 
     Parameters
     ----------
@@ -29,9 +31,11 @@ def plot_slice(X, Y, Z, vmin=None, vmax=None, skew_angle=90, ax=None,
     
     `Z`: The 2D dataset to plot.
     
-    `vmin`: The minimum value to plot in the dataset. If not provided, the minimum of the dataset will be used.
+    `vmin`: The minimum value to plot in the dataset.
+    If not provided, the minimum of the dataset will be used.
     
-    `vmax`: The maximum value to plot in the dataset. If not provided, the maximum of the dataset will be used.
+    `vmax`: The maximum value to plot in the dataset.
+    If not provided, the maximum of the dataset will be used.
     
     `skew_angle`: The angle to shear the plot in degrees. Defaults to 90 degrees (no skewing).
     
@@ -41,9 +45,11 @@ def plot_slice(X, Y, Z, vmin=None, vmax=None, skew_angle=90, ax=None,
     
     `ylim`: The limits of the y-axis. If not provided, the limits will be automatically set.
     
-    `xticks`: The major tick interval for the x-axis. If not provided, the function will use a default minor tick interval of 1.
+    `xticks`: The major tick interval for the x-axis.
+    If not provided, the function will use a default minor tick interval of 1.
     
-    `yticks`: The major tick interval for the y-axis. If not provided, the function will use a default minor tick interval of 1.
+    `yticks`: The major tick interval for the y-axis.
+    If not provided, the function will use a default minor tick interval of 1.
     
     `cbar`: Whether to include a colorbar in the plot. Defaults to True.
     
@@ -55,7 +61,8 @@ def plot_slice(X, Y, Z, vmin=None, vmax=None, skew_angle=90, ax=None,
     
     `linthresh`: The linear threshold for the symmetrical logarithmic color scale. Defaults to 1.
     
-    `title`: A string containing the title for the plot. If not provided, a default title will be used.
+    `title`: A string containing the title for the plot.
+    If not provided, a default title will be used.
 
     Returns
     -------
@@ -68,13 +75,13 @@ def plot_slice(X, Y, Z, vmin=None, vmax=None, skew_angle=90, ax=None,
     # Display Markdown heading
     if title == "None":
         pass
-    elif title != None:
+    elif title is not None:
         display(Markdown('### Figure - '+title))
     else:
         display(Markdown('### Figure'))
-        
+    
     # Inherit axes if user provides some
-    if ax != None:
+    if ax is not None:
         ax=ax
         fig=ax.get_figure()
     # Otherwise set up some default axes
@@ -83,9 +90,9 @@ def plot_slice(X, Y, Z, vmin=None, vmax=None, skew_angle=90, ax=None,
         ax = fig.add_axes([0,0,1,1])
     
     # If limits not provided, use extrema
-    if vmin==None:
+    if vmin is None:
         vmin=Z.min()
-    if vmax==None:
+    if vmax is None:
         vmax=Z.max()
 
     # Plot data
@@ -93,7 +100,7 @@ def plot_slice(X, Y, Z, vmin=None, vmax=None, skew_angle=90, ax=None,
         p = ax.pcolormesh(X, Y, Z,
                           # vmin=vmin, vmax=vmax,
                           shading='auto',
-                          norm=colors.SymLogNorm(linthresh=linthresh,vmin=-vmax, vmax=vmax),
+                          norm=colors.SymLogNorm(linthresh=linthresh, vmin=-vmax, vmax=vmax),
                           cmap=cmap
                          )
     elif logscale:
@@ -115,7 +122,7 @@ def plot_slice(X, Y, Z, vmin=None, vmax=None, skew_angle=90, ax=None,
     skew_angle = 90-skew_angle
     # Create blank 2D affine transformation
     t = Affine2D()
-    # Scale y-axis to preserve norm while shearing 
+    # Scale y-axis to preserve norm while shearing
     t += Affine2D().scale(1,np.cos(skew_angle*np.pi/180))
     # Shear along x-axis
     t += Affine2D().skew_deg(skew_angle,0)
@@ -123,7 +130,7 @@ def plot_slice(X, Y, Z, vmin=None, vmax=None, skew_angle=90, ax=None,
     t += Affine2D().scale(1,np.cos(skew_angle*np.pi/180)).inverted()
     ## Correct for x-displacement after shearing
     # If ylims provided, use those
-    if ylim != None:
+    if ylim is not None:
         # Set ylims
         ax.set(ylim=ylim)
         ymin,ymax = ylim
@@ -132,29 +139,29 @@ def plot_slice(X, Y, Z, vmin=None, vmax=None, skew_angle=90, ax=None,
         ymin,ymax = ax.get_ylim()
     # Use ylims to calculate translation (necessary to display axes in correct position)
     p.set_transform(t + Affine2D().translate(-ymin*np.sin(skew_angle*np.pi/180),0) + ax.transData)
-    
+
     # Set x limits
-    if xlim != None:
+    if xlim is not None:
         xmin,xmax = xlim
     else:
         xmin,xmax = ax.get_xlim()
     ax.set(xlim=(xmin,xmax+(ymax-ymin)/np.tan((90-skew_angle)*np.pi/180)))
     
     # Correct aspect ratio for the x/y axes after transformation
-    ax.set(aspect=np.cos(skew_angle*np.pi/180)) 
+    ax.set(aspect=np.cos(skew_angle*np.pi/180))
     
     # Add tick marks all around
     ax.tick_params(direction='in', top=True, right=True, which='both')
     
     # Set tick locations
-    if xticks==None:
+    if xticks is None:
         # Add default minor ticks
         ax.xaxis.set_minor_locator(MultipleLocator(1))
     else:
         # Otherwise use user provided values
         ax.xaxis.set_major_locator(MultipleLocator(xticks))
         ax.xaxis.set_minor_locator(MultipleLocator(1))
-    if yticks==None:
+    if yticks is None:
         # Add default minor ticks
         ax.yaxis.set_minor_locator(MultipleLocator(1))
     else:
@@ -170,9 +177,12 @@ def plot_slice(X, Y, Z, vmin=None, vmax=None, skew_angle=90, ax=None,
         if i%2:
             # Top ticks (translation here makes their direction="in")
             m._transform.set(Affine2D().translate(0,-1) + Affine2D().skew_deg(skew_angle,0))
-            # This first method shifts the top ticks horizontally to match the skew angle. This does not look good in all cases.
-            # line.set_transform(Affine2D().translate((ymax-ymin)*np.sin(skew_angle*np.pi/180),0) + line.get_transform())
-            # This second method skews the tick marks in place and can sometimes lead to them being misaligned.
+            # This first method shifts the top ticks horizontally to match the skew angle.
+            # This does not look good in all cases.
+            # line.set_transform(Affine2D().translate((ymax-ymin)*np.sin(skew_angle*np.pi/180),0) + 
+            #     line.get_transform())
+            # This second method skews the tick marks in place and
+            # can sometimes lead to them being misaligned.
             line.set_transform(line.get_transform()) # This does nothing
         else:
             # Bottom ticks
@@ -185,10 +195,6 @@ def plot_slice(X, Y, Z, vmin=None, vmax=None, skew_angle=90, ax=None,
         line =  ax.xaxis.get_minorticklines()[i]
         if i%2:
             m._transform.set(Affine2D().translate(0,-1) + Affine2D().skew_deg(skew_angle,0))
-            # This first method shifts the top ticks horizontally to match the skew angle. This does not look good in all cases.
-            # line.set_transform(Affine2D().translate((ymax-ymin)*np.sin(skew_angle*np.pi/180),0) + line.get_transform())
-            # This second method skews the tick marks in place and can sometimes lead to them being misaligned.
-            line.set_transform(line.get_transform()) # This does nothing
         else:
             m._transform.set(Affine2D().skew_deg(skew_angle,0))
         
