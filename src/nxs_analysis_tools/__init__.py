cbe4c8e029122048960e3e46e9c7b9123efff6df
Steven Gomez Alvarado <stevenjgomez@ucsb.edu>
2023-05-24 07:42:38 -0700

    Specified __all__ in __init__.py

--- src/nxs_analysis_tools/__init__.py
+++ src/nxs_analysis_tools/__init__.py
@@ -3,7 +3,7 @@ Reduce and transform nexus format (.nxs) scattering data.
 '''
 
 from _meta import __author__, __copyright__, __license__, __version__
-from .datareduction import plot_slice
+from .datareduction import plot_slice, cut_data
 
 # What to import when running "from nxs_analysis_tools import *"
-__all__ = ['plot_slice']
+__all__ = ['plot_slice','cut_data']
