efd5c121415dc0611f84bcf3b35455c9be091bd9
Steven J. Gomez Alvarado <stevenjgomez@ucsb.edu>
2023-07-06 21:45:18 -0700

    Rearrange imports

--- src/nxs_analysis_tools/__init__.py
+++ src/nxs_analysis_tools/__init__.py
@@ -2,10 +2,10 @@
 Reduce and transform nexus format (.nxs) scattering data.
 '''
 
+import numpy as np
 from _meta import __author__, __copyright__, __license__, __version__
 from .datareduction import load_data, plot_slice, Scissors, reciprocal_lattice_params
 from .chess import TempDependence
-import numpy as np
 
 # What to import when running "from nxs_analysis_tools import *"
 __all__ = ['load_data', 'plot_slice', 'Scissors', 'TempDependence',
