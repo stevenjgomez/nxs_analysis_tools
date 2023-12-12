6abb118b165a5fe041e831d30811e36d31be91ae
Steven J. Gomez Alvarado <stevenjgomez@ucsb.edu>
2023-12-11 20:52:30 -0800

    Add rotate_data to __all__

--- src/nxs_analysis_tools/__init__.py
+++ src/nxs_analysis_tools/__init__.py
@@ -4,9 +4,9 @@ Reduce and transform nexus format (.nxs) scattering data.
 
 import numpy as np
 from _meta import __author__, __copyright__, __license__, __version__
-from .datareduction import load_data, plot_slice, reciprocal_lattice_params, Scissors
+from .datareduction import load_data, plot_slice, reciprocal_lattice_params, Scissors, rotate_data
 from .chess import TempDependence
 
 # What to import when running "from nxs_analysis_tools import *"
 __all__ = ['load_data', 'plot_slice', 'Scissors', 'TempDependence',
-           'reciprocal_lattice_params']
+           'reciprocal_lattice_params', 'rotate_data']
