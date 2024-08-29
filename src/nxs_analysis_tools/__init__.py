57db0e91782bda56287ee7ebd4de51c8efd2c43e
Steven J. Gomez Alvarado <stevenjgomez@ucsb.edu>
2024-08-29 15:56:46 -0500

    Pylint

--- src/nxs_analysis_tools/__init__.py
+++ src/nxs_analysis_tools/__init__.py
@@ -4,7 +4,8 @@ Reduce and transform nexus format (.nxs) scattering data.
 
 import numpy as np
 from _meta import __author__, __copyright__, __license__, __version__
-from .datareduction import load_data, load_transform, plot_slice, reciprocal_lattice_params, Scissors, rotate_data
+from .datareduction import load_data, load_transform, plot_slice,\
+    reciprocal_lattice_params, Scissors, rotate_data
 from .chess import TempDependence
 
 # What to import when running "from nxs_analysis_tools import *"
