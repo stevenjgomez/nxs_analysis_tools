2623ca42576385bc6be892797757962d607510ef
Steven J. Gomez Alvarado <stevenjgomez@ucsb.edu>
2024-08-30 10:15:01 -0500

    Fix typo in Gaussian3DKernel

--- src/nxs_analysis_tools/pairdistribution.py
+++ src/nxs_analysis_tools/pairdistribution.py
@@ -892,7 +892,7 @@ class Gaussian3DKernel(Kernel):
         x_dim, y_dim, z_dim = size
         x = np.linspace(-x_dim, x_dim, int(x_dim) + 1)
         y = np.linspace(-y_dim, y_dim, int(y_dim) + 1)
-        z = np.linspacein(-z_dim, z_dim, int(z_dim) + 1)
+        z = np.linspace(-z_dim, z_dim, int(z_dim) + 1)
         X, Y, Z = np.meshgrid(x, y, z)
         array = np.exp(-(coeffs[0] * X ** 2 +
                          coeffs[1] * X * Y +
