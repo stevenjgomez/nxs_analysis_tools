e4239103aa3effad649efa40810bcc09592b3a5a
Steven J. Gomez Alvarado <stevenjgomez@ucsb.edu>
2025-05-26 10:12:06 -0500

    Add animation functions

--- out/nxs_analysis_tools/__init__.pyi
+++ out/nxs_analysis_tools/__init__.pyi
@@ -1,3 +1,21 @@
 from .datareduction import *
 from .chess import TempDependence as TempDependence
-from _meta import __version__ as __version__
+
+__all__ = ['load_data', 'load_transform', 'plot_slice', 'Scissors', 'reciprocal_lattice_params', 'rotate_data', 'rotate_data_2D', 'convert_to_inverse_angstroms', 'array_to_nxdata', 'Padder', 'rebin_nxdata', 'rebin_3d', 'rebin_1d', 'TempDependence', 'animate_slice_temp', 'animate_slice_axis']
+
+# Names in __all__ with no definition:
+#   Padder
+#   Scissors
+#   animate_slice_axis
+#   animate_slice_temp
+#   array_to_nxdata
+#   convert_to_inverse_angstroms
+#   load_data
+#   load_transform
+#   plot_slice
+#   rebin_1d
+#   rebin_3d
+#   rebin_nxdata
+#   reciprocal_lattice_params
+#   rotate_data
+#   rotate_data_2D
