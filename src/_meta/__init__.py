496f7cb7901f4730c0ca0975cf8d6b4b4b7fb523
Steven J. Gomez Alvarado <sjgomezalvarado@gmail.com>
2026-02-03 22:14:49 -0600

    Pivot to setuptools_scm

--- src/_meta/__init__.py
+++ src/_meta/__init__.py
@@ -1,4 +1,5 @@
 '''nxs-analysis-tools package metadata.'''
+from setuptools_scm import get_version
 
 # keep consistent with pyproject.toml
 __project__ = 'nxs-analysis-tools'
@@ -6,5 +7,5 @@ __author__ = 'Steven J. Gomez Alvarado'
 __email__ = 'stevenjgomez@ucsb.edu'
 __copyright__ = f"2023-2025, {__author__}"
 __license__ = 'MIT'
-__version__ = '0.1.14-alpha'
+__version__ = get_version(root='..', relative_to=__file__)
 __repo_url__ = 'https://github.com/stevenjgomez/nxs_analysis_tools'
